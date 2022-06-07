881b65d8dfe11bdebf309c74b8a4c2ef6a83992d
jkabat <jkabat@man.poznan.pl>
2022-06-07 15:56:16 +0200

    Add POST user_score

--- app/main.py
+++ app/main.py
@@ -1,9 +1,20 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+import logging
+from pydantic import BaseModel
+
+from fastapi import FastAPI, Request, status
+from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
+from utils import saved_user_score
+class UserData(BaseModel):
+    nick: str
+    score: float
+
 app = FastAPI(title="HexGL")
+
+logging.basicConfig(level="DEBUG")
+logger = logging.getLogger(__name__)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="static")
@@ -12,4 +23,23 @@ templates = Jinja2Templates(directory="static")
 @app.get("/", response_class=HTMLResponse())
 async def main_page(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/user_data")
+async def post_user_data(request: Request, user_data: UserData):
+    logger.debug(f"POST -> DATA: {user_data}")
+    nick = user_data.nick
+    score = user_data.score
+    score_status = saved_user_score(nick, score)
+    
+    if score_status:
+        return JSONResponse(content="Score saved", status_code=200)
+    else:
+        return JSONResponse(content="Score is lower then previos one!", status_code=409)
+    
+
+
+
+
+
+
 
---
+++ app/utils.py
@@ -0,0 +1,51 @@
+import json
+
+def save_new_nick(nick: str):
+    status = check_nickname(nick)
+    if status:
+        with open("./data/used_nicknames.txt", "w") as file:
+            file.write(nick)
+            return True
+    else:
+        print("Nickname exist.")
+        return False
+
+def check_nickname(nick: str):
+    with open("./data/used_nicknames.txt", "r") as file:
+        nicknames = file.read().split("\n")
+        print(nicknames)
+        if nick in nicknames:
+            return False
+        else: 
+            return True
+
+def saved_user_score(nick: str, score: float):
+    scores = check_score(nick, score)
+    if scores:
+        with open("./data/users_scores.json", 'w') as json_file:
+            json.dump(scores, json_file)
+            print('Score saved!')
+            return 1
+    else:
+        return 0
+
+def check_score(nick: str, score: float):
+    scores = {}
+    with open("./data/users_scores.json") as json_file:
+        scores = json.load(json_file)
+        if nick in scores.keys():
+            if scores[nick] < score:
+                scores[nick] = score
+                print(scores)
+                return scores
+            else:
+                print('Score is lower then previous one.')
+                return 0
+        else:
+            scores[nick] = score
+            print(scores)
+            return scores
+        
+
+    
+    
